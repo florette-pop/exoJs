268f25c2808661cd13122331d8aaa6328fcacad8
Florette <68685551+florette-pop@users.noreply.github.com>
2020-10-04 16:38:45 +0200

    ajout du html du site, tri du foutoire, link le js

--- exoJsPremier.js
+++ exoJsPremier.js
@@ -1,8 +1,8 @@
-let body = document.body;
-let div = document.createElement("div");
-div.id = "container";
-let h1 = document.createElement("h1");
-//h1.textContent = "The web’s most popular front-end template";
-h1.textContent = document.createTextNode('The web’s most popular front-end template');
-body.prepend(h1);
-body.appendChild(h1);
+// let body = document.body;
+// let div = document.createElement("div");
+// div.id = "container";
+// let h1 = document.createElement("h1");
+// //h1.textContent = "The web’s most popular front-end template";
+// h1.textContent = document.createTextNode('The web’s most popular front-end template');
+// body.prepend(h1);
+// body.appendChild(h1);
