fb662aa52e03da821dfcc9ec78b4e9975ff17e5e
Florette <68685551+florette-pop@users.noreply.github.com>
2020-10-05 17:35:27 +0200

    init nouvelle exrcice bouger ce ptn de carré

---
+++ colisionEvenementExo.js
@@ -0,0 +1,13 @@
+// console.log(document.querySelector('.container').getBoundingClientRect());
+//   // La méthode Element.getBoundingClientRect() renvoie la taille d'un élément et
+//   //sa position relative par rapport à la zone d'affichage (viewport).
+//
+// document.addEventListener('keypress', function(e){
+//   console.log(e.key);
+// })
+
+console.log(document.querySelector('.carre').getBoundingClientRect());
+
+document.addEventListener('keypress', function(selector){
+   console.log(selector.onclick);
+   })
