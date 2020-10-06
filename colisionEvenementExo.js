404e9333fc96562551790a762092413d580def99
Florette <68685551+florette-pop@users.noreply.github.com>
2020-10-06 09:33:44 +0200

    tentative 3

--- colisionEvenementExo.js
+++ colisionEvenementExo.js
@@ -6,21 +6,48 @@
 //   console.log(e.key);
 // })
 
-console.log(document.querySelector('.carre').getBoundingClientRect());
+var carre = document.querySelector('.carre').getBoundingClientRect();
+console.log(carre);
 
 document.addEventListener('keypress', function(e){
-   console.log(e.keyCode);
-   })
+  var dep = 50;
+  if (e.key == 'd')
+  {
+    console.log(e.key);
+    carre.x += dep;
+    console.log(carre);
+  }
+})
 
 // move('#carre')
 // .add('margin-left', 200)
 //   .end();
-
+/*
 dep=10,leftPos=0;
 
 function avancer(){
-    leftPos+=dep
-    console.log(leftPos)
-   document.getElementById("carre").style.left=leftPos+"px"
+    leftPos+=dep;
+    console.log(leftPos);
+    document.getElementById("carre").style.left=leftPos;
+}
+
+function avancer(){
+
+    var dep = 10;
+    jeu = setInterval(avancer, 100);
+
+    if(direction === DROITE){
+        document.getElementByClassName("serpent").top += dep ;
+    }
+    if(direction === GAUCHE){
+        possitionSerpent.left += dep ;
+    }
+    if(direction === HAUT){
+        possitionSerpent.top += dep ;
+    } else {
+        possitionSerpent.top -= dep ;
+    }
+
 }
 setInterval(avancer,50);
+*/
