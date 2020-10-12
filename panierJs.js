d2ab6e53a842329b0b3e5bc725cefe2be1abdb5d
Florette <68685551+florette-pop@users.noreply.github.com>
2020-10-12 14:56:57 +0200

    helped

--- panierJs.js
+++ panierJs.js
@@ -1,4 +1,20 @@
-const document.querrySelector("add").addEventListener('click', function addInBag (event) {
-  let parentPanier = querrySelector.content("add")
+// // document.querySelector("add").addEventListener('click', function addInBag (event) {
+// //   let parentPanier = document.querySelector("add")
+// //
+// // })
+//
+// document.querySelector('#add').addEventListener('click', function addPanier(e){
+//   let parentPanier = document.querySelector('#add');
+//   let product = document.querySelector('#produit');
+//   let icon = document.querySelector('#bag');
+//   parentPanier.appendChild(product);
+// })
 
+document.querySelector('#add').addEventListener('click', function addPanier(e){
+  let parentPanier = document.querySelector('#content-panier');
+  let product = document.querySelector('#produit').innerHTML;
+  let icon = document.querySelector('#panier');
+  parentPanier.appendChild(document.querySelector('#produit'));
+  console.log(parentPanier);
+  localStorage.setItem(product, product);
 })
